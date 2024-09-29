62e33919ac3ae2a29fe1d9d9c53055eb6542e79f
Aridane Alamo <aridane.alamo@rithum.com>
2024-09-29 02:05:35 +0200

    simple star background

--- Program.cs
+++ Program.cs
@@ -17,21 +17,21 @@ internal class Program
         DateTime lastFrame = DateTime.UtcNow;
         float cameraDistance = 10.0f;
         float cameraAngle = 0.0f;
-
+        var backgroundStars = GenerateStarPositions(1000, 10000f);
         var simulation = new Simulation();
         var planet = CelestialBody.Create(Vector3.Zero, 900f);
         simulation
             .AddCelestialBody(planet
-                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.EarthLike,1), size: 4f))
+                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.EarthLike, 1), size: 4f))
             .AddCelestialBody(CelestialBody
                 .Create(centralBody: planet, radius: 41f, mass: 90f, eccentricity: 0.12f, inclination: 1.67f, argumentOfPeriapsis: 1f)
-                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.Moon,2), size: 1f))
+                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.Moon, 2), size: 1f))
             .AddCelestialBody(CelestialBody
                 .Create(centralBody: planet, radius: 76f, mass: 65f, eccentricity: 0.23f, inclination: 1.73f, argumentOfPeriapsis: 2.3f)
-                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.Moon,3), size: 0.7f))
+                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.Moon, 3), size: 0.7f))
             .AddCelestialBody(CelestialBody
                 .Create(centralBody: planet, radius: 126f, mass: 44f, eccentricity: -0.17f, inclination: 0.23f, argumentOfPeriapsis: 4.2f)
-                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.IcePlanet,4), size: 0.9f));
+                .WithVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.IcePlanet, 4), size: 0.9f));
 
         while (!WindowShouldClose())
         {
@@ -41,10 +41,23 @@ internal class Program
             UpdateCamera(ref camera, ref cameraAngle, ref cameraDistance, delta_time);
             BeginDrawing();
             ClearBackground(Color.Black);
+            var t = (simulation.SimulationTime - DateTime.UnixEpoch).TotalSeconds % (Math.PI*2);
+            DrawText($"{t}",10,10,10,Color.White);
+            for (int i = 0; i < backgroundStars.Count; i++)
+            {
+                Vector3 star = backgroundStars[i];
+                var pos = GetWorldToScreen(star, camera);
+                if (pos.X < 0 || pos.X > GetScreenWidth() || pos.Y < 0 || pos.Y > GetScreenHeight())
+                {
+                    continue;
+                }
+                var s = 1f;
+                float blinkFactor = MathF.Sin((float)t+i) * 1f + 1f;
+                s *= blinkFactor;
+                DrawCircle((int)pos.X,(int)pos.Y,MathF.Max(1f, s),Color.White);
+            }
             BeginMode3D(camera);
-
             simulation.Draw(true);
-
             //Draw3DGrid(200, 5f);
             EndMode3D();
             //DrawEdit(ref orb);
@@ -52,6 +65,24 @@ internal class Program
         }
 
         CloseWindow();
+    }
+
+    static List<Vector3> GenerateStarPositions(int numberOfStars, float radius)
+    {
+        Random random = new Random();
+        List<Vector3> stars = new List<Vector3>();
+
+        for (int i = 0; i < numberOfStars; i++)
+        {
+            float theta = (float)(random.NextDouble() * 2 * Math.PI);
+            float phi = (float)(Math.Acos(2 * random.NextDouble() - 1));
+            float x = radius * MathF.Sin(phi) * MathF.Cos(theta);
+            float y = radius * MathF.Sin(phi) * MathF.Sin(theta);
+            float z = radius * MathF.Cos(phi);
+            stars.Add(new Vector3(x, y, z));
+        }
+
+        return stars;
     }
 
     static void UpdateCamera(ref Camera3D camera, ref float cameraAngle, ref float cameraDistance, float delta_time)
@@ -84,4 +115,5 @@ internal class Program
             camera.Position.Y = 2.0f; // Default Y position
         }
     }
+
 }
