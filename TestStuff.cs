87b9019591b372c337a72ef580d12ca891c3608a
Aridane Alamo <aridane.alamo@rithum.com>
2024-09-29 23:45:31 +0200

    adjust world

--- TestStuff.cs
+++ TestStuff.cs
@@ -17,15 +17,15 @@ public static class Test
             .AddCelestialBody(sun)
             .AddCelestialBody(planet)
             .AddCelestialBody(CelestialBody
-                .Create(centralBody: planet, radius: 41f, mass: 90f, eccentricity: 0.12f, inclination: 1.67f, argumentOfPeriapsis: 1f)
+                .Create(centralBody: planet, radius: 190f, mass: 90f, eccentricity: 0.12f, inclination: 1.27f, argumentOfPeriapsis: 1f)
                 .WithModelVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.Moon, 2), size: 1f)
                 .WithInfo(name: "Aeon-1A"))
             .AddCelestialBody(CelestialBody
-                .Create(centralBody: planet, radius: 76f, mass: 65f, eccentricity: 0.23f, inclination: 1.73f, argumentOfPeriapsis: 2.3f)
+                .Create(centralBody: planet, radius: 276f, mass: 65f, eccentricity: 0.13f, inclination: 1.73f, argumentOfPeriapsis: 2.3f)
                 .WithModelVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.Moon, 3), size: 0.7f)
                 .WithInfo(name: "Aeon-1B"))
             .AddCelestialBody(CelestialBody
-                .Create(centralBody: planet, radius: 126f, mass: 44f, eccentricity: -0.17f, inclination: 0.23f, argumentOfPeriapsis: 4.2f)
+                .Create(centralBody: planet, radius: 105f, mass: 44f, eccentricity: -0.27f, inclination: 0.23f, argumentOfPeriapsis: 4.2f)
                 .WithModelVisuals(model: PlanetGenerator.GeneratePlanet(PlanetSettings.IcePlanet, 4), size: 0.9f)
                 .WithInfo(name: "Aeon-1C"));
 
